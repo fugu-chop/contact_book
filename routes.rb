a7eccfa92b7494e34093e45a7256a2eb5f755b0a
Dean Wan <deanwan14@gmail.com>
2021-06-07 20:47:00 +1000

    Add: Basic routing framework

---
+++ routes.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'tilt/erubis'
+require 'dotenv/load'
+require 'bcrypt'
+require 'sinatra/content_for'
+require_relative 'app.rb'
+
+Dotenv.load
+
+configure do
+  enable(:sessions)
+  set(:session_secret, ENV['SECRET'])
+end
+
+get '/' do
+  erb(:home)
+end
+
+get '/contact' do
+  erb(:contact)
+end
