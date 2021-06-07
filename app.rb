21d510467ff5425f3b71546f779b366d92131e36
Dean Wan <deanwan14@gmail.com>
2021-06-07 21:12:00 +1000

    Add: Application logic for creating contacts

---
+++ app.rb
@@ -0,0 +1,44 @@
+def print_hello
+  'hello'
+end
+
+CONTACT_IDS = []
+
+class Book
+  attr_reader :contacts
+
+  def initialize(owner)
+    @owner = owner
+    @contacts = {}
+  end
+
+  def delete_contact(id)
+  end
+
+  def add_contact(name, phone_num, address, *category)
+    # will need some methods to extract info from Contact object
+    @contacts[generate_contact_id] =  { details: Contact.new(name, phone_num, address), category: category }
+  end
+
+  def update_contact
+  end
+
+  def display_contacts
+  end
+
+  def generate_contact_id
+    max = CONTACT_IDS.max || 0 
+    id = max + 1
+    CONTACT_IDS << id
+    id
+  end
+end
+
+class Contact
+  def initialize(name, phone_num, address)
+    @contact = { name: name, phone_number: phone_num, address: address }
+  end
+end
+
+# x = Book.new('Andy')
+# x.add_contact('james', '040404', '040404', 'lazy', 'crazy')
