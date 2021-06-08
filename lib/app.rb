dc0ff1ad17ca5f36c98d814026faf510dcf24605
Dean Wan <deanwan14@gmail.com>
2021-06-08 21:53:44 +1000

    Update: Change contact ids to object structure, add delete functionality

--- lib/app.rb
+++ lib/app.rb
@@ -2,21 +2,21 @@ def print_hello
   'hello'
 end
 
-CONTACT_IDS = []
-
 class Book
   attr_reader :contacts
 
   def initialize(owner)
     @owner = owner
     @contacts = {}
+    @contact_ids = []
   end
 
   def delete_contact(id)
+    @contacts.delete(id)
   end
 
   def add_contact(name, phone_num, address, *category)
-    @contacts[generate_contact_id] =  { details: Contact.new(name, phone_num, address) }
+    @contacts[generate_contact_id] =  { details: Contact.new(name, phone_num, address, category) }
   end
 
   def update_contact
@@ -29,9 +29,9 @@ class Book
   end
 
   def generate_contact_id
-    max = CONTACT_IDS.max || 0 
+    max = @contact_ids.max || 0 
     id = max + 1
-    CONTACT_IDS << id
+    @contact_ids << id
     id
   end
 end
@@ -44,9 +44,10 @@ class Contact
     @name = name
     @phone_number = phone_num
     @address = address
-    @category = category
+    @category = category.flatten
   end
 end
 
 # x = Book.new('Andy')
 # x.add_contact('james', '040404', '040404', 'lazy', 'crazy')
+# x[1][:details].category.size
